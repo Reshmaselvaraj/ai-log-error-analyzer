9b19e29e1a094cfb0b59444e5644438e0c0166ed
Reshmaselvaraj <reshmaselvaraj02@gmail.com>
2026-01-21 14:31:40 +0530

    Point Streamlit frontend to hosted FastAPI backend

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/analyze"
+API_URL = "API_URL = "https://ai-log-error-analyzer.onrender.com/analyze"
 
 st.title("AI Log Error Analyzer")
 
